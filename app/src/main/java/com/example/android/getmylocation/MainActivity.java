67205e4e14a45549e6afae324b4410be0974d1ae
Konstantin Pavlov <konstpavlov87@gmail.com>
2018-09-15 01:05:17 +0400

    - all strings added to string resources - alternative methods were commented

--- app/src/main/java/com/example/android/getmylocation/MainActivity.java
+++ app/src/main/java/com/example/android/getmylocation/MainActivity.java
@@ -15,10 +15,10 @@ import java.util.concurrent.TimeUnit;
 public class MainActivity extends AppCompatActivity  {
 
     TextView result;
-    String s;
-
+    String s, latide, longde;
+    double latitude,  longitude;
+    long i, before, after ;
     private GpsTracker gpsTracker;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +26,68 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
 
         result = (TextView) findViewById(R.id.coordinates);
-        //s = "failed";
+        latide = getString(R.string.latitude);
+        longde = getString(R.string.longitude);
+
     }
 
 
     public void get_coordinates (View view) {
+
+        //вариант с использованием текущего времени (работает в java проекте, но не здесь)
+        before = System.currentTimeMillis();
+        i = before;
+        while (true) {
+
+            after = System.currentTimeMillis();
+            if (after - before > 1000) {
+
+                before = System.currentTimeMillis();
+
+                gpsTracker = new GpsTracker(MainActivity.this);
+                if (gpsTracker.canGetLocation()) {
+                    latitude = gpsTracker.getLatitude();
+                    longitude = gpsTracker.getLongitude();
+                    s += latide + latitude + "\n" + longde + longitude + "\n\n";
+                    display(s);
+
+                } else {
+                    gpsTracker.showSettingsAlert();
+                    s = getString(R.string.failed);
+                }
+            }
+
+            if (before - i > 10000) break;
+        }
+    }
+
+
+    public void display(String str) {
+        result.setText(str);
+    }
+}
+
+        /*
+        //вариант с Thread
+        try {
+            Thread.sleep(1000); //Приостанавливает поток на 1 секунду
+        }
+        catch (Exception e) {}
+        */
+
+
+/*
+*
+*       //вариант без фонового отображения (без цикла)
+        gpsTracker = new GpsTracker(MainActivity.this);
+        if(gpsTracker.canGetLocation()){
+            double latitude = gpsTracker.getLatitude();
+            double longitude = gpsTracker.getLongitude();
+            s += "latitude: " + latitude + "\n" + "longitude: " + longitude + "\n\n";
+
+        }
+        display(s);
+* */
 
         /*
         //просто тестовый вариант, без высчитывания реальных координат (работает в java проекте, но не здесь)
@@ -50,63 +107,3 @@ public class MainActivity extends AppCompatActivity  {
             }
         }
         */
-
-        /*
-        //вариант с использованием текущего времени (работает в java проекте, но не здесь)
-        long i ;
-        int a=0, b=0;
-        long before = System.currentTimeMillis();
-        i = before;
-        while (true) {
-
-
-           try {
-                Thread.sleep(1000); //Приостанавливает поток на 1 секунду
-            }
-            catch (Exception e) {}
-
-
-            long after = System.currentTimeMillis();
-            if (after - before > 1000) {
-
-                before = System.currentTimeMillis();
-
-                gpsTracker = new GpsTracker(MainActivity.this);
-                if (gpsTracker.canGetLocation()) {
-                    //double latitude = gpsTracker.getLatitude();
-                    //double longitude = gpsTracker.getLongitude();
-                    double latitude = a++;
-                    double longitude = b++;
-                    s += "latitude: " + latitude + "\n" + "longitude: " + longitude + "\n\n";
-                    display(s);
-
-                } else {
-                    gpsTracker.showSettingsAlert();
-                    s = "failed";
-                }
-            }
-
-            if (before - i > 10000) break;
-        }*/
-    }
-
-
-    public void display(String str) {
-        //TextView result = (TextView) findViewById(R.id.result);
-        result.setText(str);
-    }
-}
-
-
-/*
-*
-*       //вариант без фонового отображения (без цикла)
-        gpsTracker = new GpsTracker(MainActivity.this);
-        if(gpsTracker.canGetLocation()){
-            double latitude = gpsTracker.getLatitude();
-            double longitude = gpsTracker.getLongitude();
-            s += "latitude: " + latitude + "\n" + "longitude: " + longitude + "\n\n";
-
-        }
-        display(s);
-* */
