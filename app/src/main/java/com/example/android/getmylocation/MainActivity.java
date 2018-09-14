fc2a96f2e85b04849803186480f5cd37801e7051
Konstantin Pavlov <konstpavlov87@gmail.com>
2018-09-14 17:59:49 +0400

    - for test purpose added several options to get_coordinates method

--- app/src/main/java/com/example/android/getmylocation/MainActivity.java
+++ app/src/main/java/com/example/android/getmylocation/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.widget.TextView;
 import android.location.LocationListener;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity  {
 
     TextView result;
@@ -30,16 +32,38 @@ public class MainActivity extends AppCompatActivity  {
 
     public void get_coordinates (View view) {
 
+        /*
+        //просто тестовый вариант, без высчитывания реальных координат (работает в java проекте, но не здесь)
+        int timeToWait = 10; //second
+        int a=0, b=0;
+
+        for (int i=0; i<timeToWait ; i++) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                double latitude = a++;
+                double longitude = b++;
+                s += "latitude: " + latitude + "\n" + "longitude: " + longitude + "\n\n";
+                display(s);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        */
+
+        /*
+        //вариант с использованием текущего времени (работает в java проекте, но не здесь)
         long i ;
+        int a=0, b=0;
         long before = System.currentTimeMillis();
         i = before;
         while (true) {
 
 
-/*            try {
+           try {
                 Thread.sleep(1000); //Приостанавливает поток на 1 секунду
             }
-            catch (Exception e) {}*/
+            catch (Exception e) {}
 
 
             long after = System.currentTimeMillis();
@@ -49,8 +73,10 @@ public class MainActivity extends AppCompatActivity  {
 
                 gpsTracker = new GpsTracker(MainActivity.this);
                 if (gpsTracker.canGetLocation()) {
-                    double latitude = gpsTracker.getLatitude();
-                    double longitude = gpsTracker.getLongitude();
+                    //double latitude = gpsTracker.getLatitude();
+                    //double longitude = gpsTracker.getLongitude();
+                    double latitude = a++;
+                    double longitude = b++;
                     s += "latitude: " + latitude + "\n" + "longitude: " + longitude + "\n\n";
                     display(s);
 
@@ -61,7 +87,7 @@ public class MainActivity extends AppCompatActivity  {
             }
 
             if (before - i > 10000) break;
-        }
+        }*/
     }
 
 
@@ -74,6 +100,7 @@ public class MainActivity extends AppCompatActivity  {
 
 /*
 *
+*       //вариант без фонового отображения (без цикла)
         gpsTracker = new GpsTracker(MainActivity.this);
         if(gpsTracker.canGetLocation()){
             double latitude = gpsTracker.getLatitude();
